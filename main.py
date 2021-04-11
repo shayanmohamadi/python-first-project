2ff807f03ede8e5696e8a12848de0663949a3e19
shayanmohamadi <shayanmohamadi735@gmail.com>
2021-04-11 01:15:19 -1000

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 def main():
-    driver = webdriver.Firefox('')
+    #driver = webdriver.Firefox('')
+    driver = webdriver.Firefox()
+    driver.get('http://google.com')
+ 
 if '__main__'==__name__:
     main()
